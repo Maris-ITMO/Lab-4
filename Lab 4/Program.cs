9a96494cc10fae93fd10310b14beea54372c798e
Maris Shapiro <19maris72@gmail.com>
2023-01-07 01:59:47 +0300

    убрана команда прерывания цикла

--- Lab 4/Program.cs
+++ Lab 4/Program.cs
@@ -12,8 +12,7 @@ namespace Lab_4
         {
             for (int i = 0; i < 10; i++)
             {
-                if (i == 5)
-                    continue;
+                
                 Console.WriteLine("Квадрат числа {0} равен {1}", i, i * i);
             }
             
